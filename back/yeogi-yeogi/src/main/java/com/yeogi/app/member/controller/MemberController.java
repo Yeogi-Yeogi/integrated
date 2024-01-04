3b06394f71f27603cdf7ee9eae75a1361a6c1982
DESKTOP-URCCHTU\sales <llds0216@naver.com>
2024-01-04 20:48:48 +0900

    회원가입기능 구현중

--- back/yeogi-yeogi/src/main/java/com/yeogi/app/member/controller/MemberController.java
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/member/controller/MemberController.java
@@ -1,5 +1,27 @@
 package com.yeogi.app.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yeogi.app.member.service.MemberService;
+import com.yeogi.app.member.vo.MemberVo;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
+	
+	private final MemberService service;
+	
+	//회원가입
+	@PostMapping("join")
+	public void join(MemberVo vo) {
+		int result = service.join(vo);
+		
+	}
+	
 
 }
--- back/yeogi-yeogi/src/main/java/com/yeogi/app/member/dao/MemberDao.java
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/member/dao/MemberDao.java
@@ -1,5 +1,16 @@
 package com.yeogi.app.member.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.yeogi.app.member.vo.MemberVo;
+
+@Repository
 public class MemberDao {
+
+	//회원가입
+	public int join(SqlSessionTemplate sst, MemberVo vo) {
+		return sst.insert("MemberMapper.join", vo);		
+	}
 
 }
