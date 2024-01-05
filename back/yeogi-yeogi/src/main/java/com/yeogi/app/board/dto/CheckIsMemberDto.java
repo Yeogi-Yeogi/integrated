e3659ee3a43f41634e87c2b2495f28a95b13edf5
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-05 20:15:33 +0900

    feat: 모임에 가입한 사람인지 검사하는 로직 추가

--- back/yeogi-yeogi/src/main/java/com/yeogi/app/board/dto/CheckIsMemberDto.java
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/board/dto/CheckIsMemberDto.java
@@ -7,8 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BoardDetailValidDto {
+public class CheckIsMemberDto {
     private String memberNo;
     private String clubNo;
-    private String boardNo;
 }
---
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/util/valid/CheckClubMember.java
@@ -0,0 +1,28 @@
+package com.yeogi.app.util.valid;
+
+import com.yeogi.app.board.dto.CheckIsMemberDto;
+import com.yeogi.app.club.dao.ClubDao;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configuration
+@Transactional
+@RequiredArgsConstructor
+public class CheckClubMember {
+
+    private final ClubDao clubRepository;
+
+    private final SqlSessionTemplate template;
+
+    /**
+     * 모임에 가입한 사람인지 검사하는 로직
+     * @param dto
+     * @return
+     */
+    public boolean isClubMember(CheckIsMemberDto dto) {
+        String findMemberNo = clubRepository.checkIsClubMember(dto, template);
+        return findMemberNo == null ? false : true;
+    }
+}
