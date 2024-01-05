7cb50a07bf919097ff70194b1012fd2b39af2a15
JB <whdqja918@gmail.com>
2024-01-05 16:29:31 +0900

    240105 쪼금수정

---
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/club/dto/EditClubDto.java
@@ -0,0 +1,13 @@
+package com.yeogi.app.club.dto;
+
+import lombok.Data;
+
+@Data
+public class EditClubDto {
+
+    // 대표이미지 변경, 소개글 변경, 모임인원 변경, 가입 연령 변경,
+    // 회원관리 ( 관리자 지정, 추방? )
+    private String asdf;
+
+
+}
