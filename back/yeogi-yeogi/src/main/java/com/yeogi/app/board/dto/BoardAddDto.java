f5117b978f34cef33d19df766ed5e3c3d13fa5ef
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-05 20:11:20 +0900

    feat: 게시글 작성 기능 추가(테스트 안함)

--- back/yeogi-yeogi/src/main/java/com/yeogi/app/board/dto/BoardAddDto.java
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/board/dto/BoardAddDto.java
@@ -1,5 +1,6 @@
 package com.yeogi.app.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,8 +10,11 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class BoardAddDto {
+    private String clubNo;
+    private String memberNo;
     private String title;
-    private Lob content;
+    private String content;
     private List<MultipartFile> imageList;
 }
