1c3fb5235fdea64ce1dbdeb00a81f4a523224f47
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-08 08:43:36 +0900

    refactor: 코드 구조 수정

---
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/util/check/CheckDto.java
@@ -0,0 +1,15 @@
+package com.yeogi.app.util.check;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckDto {
+    private String clubNo;
+    private String memberNo;
+    private String creatorYn;
+    private String adminYn;
+}
