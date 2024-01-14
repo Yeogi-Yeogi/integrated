6f69390773abfe3aa345a1a4606d9bae8c563a3a
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-14 22:47:17 +0900

    style: 공지사항 작성 페이지 구현 중(일정 추가만 남음)

--- front/yeogi-yeogi/src/component/club/comunnity/notice/NoticeLayout.jsx
+++ front/yeogi-yeogi/src/component/club/comunnity/notice/NoticeLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NoticeList from './NoticeList';
 import NoticeDetail from './NoticeDetail';
+import NoticeWrite from './NoticeWrite';
 
 const NoticeLayout = () => {
     return (
         <Routes>
             <Route path='list' element={<NoticeList />}/>
             <Route path='detail' element={<NoticeDetail />}/>
+            <Route path='write' element={<NoticeWrite />}/>
         </Routes>
     );
 };
