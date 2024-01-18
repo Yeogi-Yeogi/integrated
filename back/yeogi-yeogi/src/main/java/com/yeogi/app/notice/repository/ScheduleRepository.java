4e3214cbc4e6f320679c168452abf01c7eef488d
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-18 16:35:29 +0900

    feat & fix: 일정 목록 조회 기능 추가 및 공지사항 상세조회 버그 수정

--- back/yeogi-yeogi/src/main/java/com/yeogi/app/notice/repository/ScheduleRepository.java
+++ back/yeogi-yeogi/src/main/java/com/yeogi/app/notice/repository/ScheduleRepository.java
@@ -1,8 +1,12 @@
 package com.yeogi.app.notice.repository;
 
 import com.yeogi.app.notice.vo.ScheduleVo;
+import com.yeogi.app.util.check.CheckDto;
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ScheduleRepository {
@@ -33,5 +37,28 @@ public class ScheduleRepository {
      */
     public int deleteSchedule(String boardNo, SqlSessionTemplate template) {
         return template.delete("ScheduleMapper.deleteByBoardNo", boardNo);
+    }
+
+    /**
+     * 예정 일정 리스트
+     *
+     * @param dto
+     * @param template
+     * @param rowBounds
+     * @return
+     */
+    public List<ScheduleVo> getUpComingListByClubNo(CheckDto dto, SqlSessionTemplate template, RowBounds rowBounds) {
+        return template.selectList("ScheduleMapper.getUpComingList", dto, rowBounds);
+    }
+
+    /**
+     * 지난 일정 리스트
+     * @param dto
+     * @param template
+     * @param rowBounds
+     * @return
+     */
+    public List<ScheduleVo> getPastListByClubNo(CheckDto dto, SqlSessionTemplate template, RowBounds rowBounds) {
+        return template.selectList("ScheduleMapper.getPastList", dto, rowBounds);
     }
 }
--- front/yeogi-yeogi/src/component/club/comunnity/schedule/ScheduleUpcoming.jsx
+++ front/yeogi-yeogi/src/component/club/comunnity/schedule/ScheduleUpcoming.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import ScheduleItem from './ScheduleItem';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button, Spinner } from 'react-bootstrap';
 
 const StyledScheduleLisDiv = styled.div`
     width: 100%;
@@ -12,12 +14,80 @@ const StyledScheduleLisDiv = styled.div`
 const ScheduleUpcoming = () => {
 
      //데이터 받아오기
+    const [page, setPage] = useState(-1); //페이징 번호
+    const [load, setLoad] = useState(false); //==isFetching
+    const [dto, setDto] = useState([]); //리스트
+    const [isAdmin, setIsAdmin] = useState();
+    const preventRef = useRef(true);
+    const obsRef = useRef(null); //옵저버 element
+    const endRef = useRef(false); //모든 게시글 리스트 가져왔는지
+    const {clubNo} = useParams();
+    const navigate = useNavigate();
+    const vo = JSON.parse(sessionStorage.getItem("loginMember"));
+    const memberNo = vo?.no;
+
+    useEffect(() => {
+        if(memberNo) {
+            const observer = new IntersectionObserver(obsHandler, {threshold : 0.5});
+            if(obsRef.current) observer.observe(obsRef.current);
+            return () => {observer.disconnect(); }
+        } else {
+            alert('로그인한 회원만 이용가능합니다');
+            navigate('/member/login');
+        }
+    }, []);
+
+    const obsHandler = (entries) => {
+        const target = entries[0];
+        if(!endRef.current && target.isIntersecting && preventRef.current) {
+            preventRef.current = false;
+            setPage(prev => prev+1);
+        }
+    }
+
+    useEffect(() => {
+        if(page !== -1) getPost();
+     }, [page]);
+ 
+     const getPost = useCallback(async () => {
+         setLoad(true); //로딩 시작 
+ 
+         try {
+             const res = await fetch(`http://localhost:8885/schedule/list/${page}?memberNo=${memberNo}&clubNo=${clubNo}&isExpected=Y`);
+             const data = await res.json();
+             console.log(data);
+             if(data.length === 0) { //마지막 페이지일 경우
+                 endRef.current = true;
+             }
+ 
+             setDto(prev => [...prev, ...data.list]);
+             setIsAdmin(data.admin);
+             preventRef.current = true;
+         } catch (e) {
+             console.log(e);
+         } finally {
+             setLoad(false);
+         }
+     }, [page]);
 
     return (
         <StyledScheduleLisDiv>
-            <ScheduleItem/>
-            <ScheduleItem/>
-            <ScheduleItem/>
+            {
+                isAdmin &&
+                <div style={{textAlign:'right' }}>
+                    <Button>일정 등록하기</Button>
+                </div>
+            }
+            {
+                dto?.map(el => 
+                    <ScheduleItem data={el} key={el.scheduleNo}/>
+                )
+            }
+            {
+                load &&
+                <Spinner animation="border" />
+            }
+            <div ref={obsRef}></div>
         </StyledScheduleLisDiv>
     );};
 
