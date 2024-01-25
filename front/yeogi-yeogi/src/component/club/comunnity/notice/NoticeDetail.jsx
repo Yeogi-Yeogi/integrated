b3ab808620f72fbd84be3eaa6dc03e2d76d9576a
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-25 14:14:09 +0900

    fix: 에러 메시지에 따라 분기 처리 추가

--- front/yeogi-yeogi/src/component/club/comunnity/notice/NoticeDetail.jsx
+++ front/yeogi-yeogi/src/component/club/comunnity/notice/NoticeDetail.jsx
@@ -163,8 +163,20 @@ const NoticeDetail = () => {
                 console.log(data);
                 setNotice(data);
             })
-            .catch(err => {
-                alert(err.mssage);
+            .catch(e => {
+                const message = e.message;
+                alert(message);
+                switch(message) {
+                    case "회원 전용 서비스입니다. 로그인하세요.":
+                        navigate('/member/login');
+                        break;
+                    case "해당 공지사항이 없습니다":
+                        navigate(`/club/${clubNo}/commu/board/notice/list`);
+                        break;
+                    default:
+                        navigate("/main");
+                        break;
+                }
             })
         } else {
             alert('로그인한 회원만 이용가능합니다');
